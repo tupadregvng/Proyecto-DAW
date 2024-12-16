01fe8dd6e6283fa3c4f98e9d1a1ccc16529ee935
Tommyoly <oliverebasatomas@gmail.com>
2024-12-16 22:45:21 +0100

    Reajustados los estilos de dashboards cliente y admin. Creado nuevos CSS: uno general para todas las páginas y otros particulares de cada página. Añadido dos formularios para crear nuevos clientes y nuevos proveedores con botones de redirección

--- indexAdmin.php
+++ indexAdmin.php
@@ -3,14 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilosGenerales.css">
     <link rel="stylesheet" href="indexAdmin.css">
     <title>Panel de administradores</title>
 </head>
-<header>
-    <h1>Panel de Administradores</h1>
-</header>
+
 <body>
-    
 
     <!-- Menú Lateral (Sidebar) -->
     <div class="sidebar">
@@ -22,24 +20,31 @@
 
     <!-- Contenido Principal -->
     <div class="main-content">
+        <div class="header">
+            <h1>Panel de Administradores</h1>
+        </div>
         <section id="clientes">
             <h2>Clientes</h2>
             <p>Aquí podrás gestionar todos los clientes registrados en el sistema.</p>
+            <button onclick="window.location.href='nuevoCliente.php';">Añadir nuevo cliente</button>
         </section>
 
         <section id="proveedores">
             <h2>Proveedores</h2>
             <p>Aquí podrás gestionar los proveedores asociados a tu empresa.</p>
+            <button onclick="window.location.href='nuevoProveedor.php';">Añadir nuevo proveedor</button>
         </section>
 
         <section id="stock">
             <h2>Stock</h2>
             <p>Aquí podrás ver y administrar el inventario de productos o piezas disponibles.</p>
+            <button onclick="window.location.href='nuevoRecambio.php';">Añadir nuevo recambio</button>
         </section>
 
         <section id="citas">
             <h2>Citas</h2>
             <p>Aquí podrás gestionar las citas de los clientes y las reparaciones pendientes.</p>
+            <button onclick="window.location.href='citas.php';">Gestionar citas</button>
         </section>
     </div>
 </body>
--- indexCliente.php
+++ indexCliente.php
@@ -3,6 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilosGenerales.css">
     <link rel="stylesheet" href="indexCliente.css">
     <title>Cliente</title>
 </head>
