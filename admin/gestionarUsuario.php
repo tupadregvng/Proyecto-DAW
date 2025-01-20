49fdad899f1015f06d80179d80ec9114eb8754ee
Tommyoly <oliverebasatomas@gmail.com>
2025-01-20 21:23:36 +0100

    Añadido denegación de acceso a los que no son admin si intenta acceder a traves de url

--- admin/gestionarUsuario.php
+++ admin/gestionarUsuario.php
@@ -1,4 +1,12 @@
 <?php
+    session_start(); // Iniciar sesión
+    $nombre = $_SESSION['name'];
+    $role = $_SESSION['role'];
+    if(!isset($nombre) || $role !== 1){
+        echo "<script>alert('No tienes acceso a esta página.'); window.location.href = '../index.php';</script>";
+    }
+?>
+<?php
 // Requiere MongoDB
 require '../vendor/autoload.php';
 
--- admin/nuevaMoto.php
+++ admin/nuevaMoto.php
@@ -1,3 +1,11 @@
+<?php
+    session_start(); // Iniciar sesión
+    $nombre = $_SESSION['name'];
+    $role = $_SESSION['role'];
+    if(!isset($nombre) || $role !== 1){
+        echo "<script>alert('No tienes acceso a esta página.'); window.location.href = '../index.php';</script>";
+    }
+?>
 <!DOCTYPE html>
 <html lang="es">
 <head>
